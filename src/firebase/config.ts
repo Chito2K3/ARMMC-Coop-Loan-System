d2f7ab14092e06c04bdb46b8cae4b1e66ed4a914
Chito Saba <chitosaba@gmail.com>
2025-11-23 01:09:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5465662613-46f9e",
+  "appId": "1:368018377196:web:9020da7bacf2a946ff98c9",
+  "apiKey": "AIzaSyBnSQ5vTAiBwFQdEizXGBKqbtNAmbmLo-8",
+  "authDomain": "studio-5465662613-46f9e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "368018377196"
+};
